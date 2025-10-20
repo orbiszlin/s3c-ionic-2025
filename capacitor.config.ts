3e12ec831a6fefaaa8cd2a91c4f15fd4838b5e53
jakubforman <me@jakubforman.eu>
2025-10-20 08:53:59 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'showcase-s3c',
+  webDir: 'www'
+};
+
+export default config;
