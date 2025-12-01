20f88261fa47ac4c6e8097882e4295d9dab5f3ed
jakubforman <me@jakubforman.eu>
2025-12-01 10:44:15 +0100

    Refactor `WeatherService` to use typed Location model and observable naming conventions, integrate modal example in `WeatherPage`, and move `Location` interface to a standalone model file.

--- src/app/pages/weather/weather.page.ts
+++ src/app/pages/weather/weather.page.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, ViewChild} from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -7,53 +7,19 @@ import {
   IonList,
   IonItem,
   IonBadge,
-  IonLabel, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonRow, IonCol
+  IonLabel, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonRow, IonCol, IonModal,
+  IonButtons, IonInput
 } from '@ionic/angular/standalone';
 import {ExploreContainerComponent} from '../../explore-container/explore-container.component';
 import {WeatherService} from "../../utils/services/weather/weather.service";
-
-export interface Location {
-  name: string;
-  weather: {
-    id: number;
-    main: number;
-    description: number;
-    icon: number;
-  }[];
-  base: string;
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-    sea_level: number;
-    grnd_level: number;
-  },
-  visibility: number;
-  wind: {
-    speed: number;
-    deg: number;
-  },
-  clouds: {
-    all: number;
-  },
-  dt: number;
-  sys: {
-    type: number;
-    id: number;
-    country: string;
-    sunrise: number;
-    sunset: number;
-  }
-}
+import {Location} from "../../models/location.model";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'weather.page.html',
   styleUrls: ['weather.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonList, IonItem, IonBadge, IonLabel, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonRow, IonCol],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonList, IonItem, IonBadge, IonLabel, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonRow, IonCol, IonModal, IonButtons, IonInput, FormsModule],
 })
 export class WeatherPage {
 
@@ -62,10 +28,29 @@ export class WeatherPage {
   public locations: Location[] = [];
 
   constructor() {
-    this.weatherService.getWeather(40.7128, -74.0060)
+    this.weatherService.getWeather$(40.7128, -74.0060)
       .subscribe(data => {
-        this.locations.push(data as Location);
+        this.locations.push(data);
         // console.log(data);
       });
+  }
+
+  @ViewChild(IonModal) modal!: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name!: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: CustomEvent<any>) {
+    if (event.detail.role === 'confirm') {
+      this.message = `Hello, ${event.detail.data}!`;
+    }
   }
 }
--- src/app/utils/services/weather/weather.service.ts
+++ src/app/utils/services/weather/weather.service.ts
@@ -1,6 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
+import {Location} from "../../../models/location.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class WeatherService {
 
   private http = inject(HttpClient);
 
-  getWeather(lat: number, lon: number) {
-    return this.http.get(environment.baseUrl + `/weather?lat=${lat}&lon=${lon}&appid=${environment.apiKey}`);
+  getWeather$(lat: number, lon: number) {
+    return this.http.get<Location>(environment.baseUrl + `/weather?lat=${lat}&lon=${lon}&appid=${environment.apiKey}&units=metric`);
   }
 }
